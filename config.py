01ea3e25659e1daa26581f48df668952f1ef0237
Prince Rawat <princerawat7700@gmail.com>
2025-08-19 21:21:14 +0530

    fix: remove hardcoded bot token from config

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ from logging.handlers import RotatingFileHandler
 load_dotenv()
 
 #Bot token @Botfather
-TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "8210267419:AAHSHLjc4253KGWQrEUl1yU-GLdAVLNmO1A")
+TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
 
 #Your API ID from my.telegram.org
 APP_ID = int(os.environ.get("APP_ID", "21726327"))
